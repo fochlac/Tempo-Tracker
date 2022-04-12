0f43b5e914c07bcccaf2c019668e03f1cefebf5a
Florian Riedel <florian.riedel@webfleet.com>
2022-04-12 16:23:27 +0200

    add own logo

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -4,17 +4,20 @@ import { useOptions } from "../../hooks/useOptions"
 import { viewDuck } from "../../store/ducks/view"
 import { useSelector } from "../../utils/atom"
 import { InternalLink } from "../atoms/InternalLink"
+import { Logo } from "../atoms/Logo"
 import { Tooltip } from "../atoms/Tooltip"
 
 const AppBar = styled.header`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
-    padding: 8px 16px;
+    justify-content: flex-start;
+    padding: 8px 16px 8px 8px;
 `
 const Title = styled.h1`
     font-size: 1.2rem;
+    margin-right: auto;
+    margin-left: 4px;
 `
 
 export const Header:React.FC = () => {
@@ -25,6 +28,7 @@ export const Header:React.FC = () => {
 
     return (
         <AppBar>
+            <Logo style={{width: 24, height: 24}} />
             <Title>Tempo-Tracker</Title>
             {view === VIEWS.TRACKER && <InternalLink to={VIEWS.OPTIONS}>Options</InternalLink>}
             {view === VIEWS.OPTIONS && (
--- src/utils/background.js
+++ src/utils/background.js
@@ -4,18 +4,23 @@ const controller = chrome || browser
 
 export const triggerBackgroundAction = (action) => {
     return new Promise((resolve, reject) => {
-        const errTimer = setTimeout(() => {
-            reject(ACTIONS[action.type].response(false, 'Action timed out after 60 seconds.'))
-        }, 60000)
-        controller.runtime.sendMessage(action, function (response) {
-            clearTimeout(errTimer)
-            if (response.payload.success) {
-                resolve(response.payload)
-            }
-            else {
-                reject(response.payload)
-            }            
-        });
+        if (controller?.runtime?.sendMessage) {
+            const errTimer = setTimeout(() => {
+                reject(ACTIONS[action.type].response(false, 'Action timed out after 60 seconds.'))
+            }, 60000)
+            controller.runtime?.sendMessage(action, function (response) {
+                clearTimeout(errTimer)
+                if (response.payload.success) {
+                    resolve(response.payload)
+                }
+                else {
+                    reject(response.payload)
+                }            
+            });
+        }
+        else {
+            reject(ACTIONS[action.type].response(false, 'Could not send message.'))
+        }
     })
 }
 export function checkTabExistence(tabId) {
