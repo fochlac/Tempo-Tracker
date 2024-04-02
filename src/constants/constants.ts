11ec17c4268faccd00a0c6029100669902fd263b
Florian Riedel <florian.riedel@webfleet.com>
2024-04-02 16:14:58 +0200

    extend support for different domain types

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,10 @@
-export const VIEWS: Record<string, VIEWS> =  {
+export const VIEWS: Record<string, VIEWS> = {
     STATS: 'stats',
     TRACKER: 'tracker',
     OPTIONS: 'options'
 }
 
-export const CACHE: Record<string, CACHE> =  {
+export const CACHE: Record<string, CACHE> = {
     WORKLOG_CACHE: 'WORKLOG_CACHE',
     STATS_CACHE: 'STATS_CACHE',
     LIFETIME_STATS_CACHE: 'LIFETIME_STATS_CACHE',
@@ -24,7 +24,7 @@ export const CACHE_STORE = 'CACHE_STORE'
 export const DATABASE_NAME = 'tempo-tracker'
 
 export const atlassianRegexp = /(https?:\/\/|^)([\w-]+\.atlassian\.\w+)(\/|$)/
-export const domainRegexp = /(https?:\/\/|^)(([\w-]+\.|)[\w-]+\.\w+)(\/|$)/
+export const domainRegexp = /(https?:\/\/|^)(([\w-]+\.)*([\w-]+\.)?\w+)(\/|$)/
 
 export const THEMES: THEMES = {
     DEFAULT: 'DEFAULT',
